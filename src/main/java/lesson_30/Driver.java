7b67e2f9a35719b3eece5b4557ec678d8d287d98
faxcon08 <faxcon-08@mail.ru>
2023-05-10 15:37:58 +0300

    add New Branch

--- src/main/java/lesson_30/Driver.java
+++ src/main/java/lesson_30/Driver.java
@@ -1,6 +1,34 @@
 package lesson_30;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Driver {
     private String name;
+    @Autowired
     private Transport transport;
+
+    public Driver(String name) {
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Transport getTransport() {
+        return transport;
+    }
+
+    public void setTransport(Transport transport) {
+        this.transport = transport;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver [ "+name+" ] with "+transport;
+    }
 }
